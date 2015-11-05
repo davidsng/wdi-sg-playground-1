6005c6af07f6df75a4dc03e9cfb32cc94fd3258b
Albert <albertlimca@gmail.com>
2015-11-05 17:22:43 +0800

    js-playground

---
+++ js-playground/demo-classes.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor () {
+    this.luckyNumber = Math.random()
+  }
+  greeting () {
+    console.log('hello ' + name)
+  }
+}
+
+var bert = new Person()
+seb.greeting('you')
