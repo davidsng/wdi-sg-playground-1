d544134383729f87674bf7fd983199711e3615f0
Albert <albertlimca@gmail.com>
2015-11-08 15:03:12 +0800

    Add reset button and remove event listener

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -1,61 +1,85 @@
-var player = true
-var body = document.querySelector('body')
-var moves = 1
-var winner = ''
+// select HTML elements and store them in variables
+var body = document.querySelector('body') // body element to listen for clicks
+var title = document.querySelector('.title') // title bar
+var tiles = Array.prototype.slice.call(document.querySelectorAll('.tile')) // monitor tiles for 'X' and 'O'
+var reset = document.querySelector('.reset') // reset button to reset the game
+// create variables for the tic tac toe game
+var player, moves, winner
+resetBoard()
 
-body.addEventListener('click', function (event) {
+// Event listener on body for tictactoe moves
+body.addEventListener('click', function tictactoe (event) {
   var tile = event.target
   if (tile.className !== 'tile') return
   if (tile.textContent) return
   player ? mark(tile, 'X') : mark(tile, 'O')
+  // find winner by checking tiles for 'X' and 'O'
   findWinner()
-  changePlayer()
+  if (winner) {
+    title.textContent = winner + ' wins!'
+    body.removeEventListener('click', tictactoe)
+    reset.classList.remove('hidden')
+  } else {
+    if (moves === 9) {
+      // it's a tie
+      title.textContent = 'It\'s a tie!'
+    } else {
+      moves = moves + 1
+      changePlayer()
+    }
+  }
+})
+// Event listener on reset button
+reset.addEventListener('click', function () {
+  resetBoard()
 })
 
+// function to mark 'X'/'O' when a tile is clicked
 function mark (tile, mark) {
   tile.textContent = mark
 }
+// function to change player and update the turn in title bar
 function changePlayer () {
   player = !player
+  updateTitle()
 }
 
 function findWinner () {
-  var tiles = document.querySelectorAll('.tile')
   // tiles layout by index
   // [0] [1] [2]
   // [3] [4] [5]
   // [6] [7] [8]
-
-  // check for 3 in a row
-  checkTiles(tiles[0], tiles[1], tiles[2])
-  checkTiles(tiles[3], tiles[4], tiles[5])
-  checkTiles(tiles[6], tiles[7], tiles[8])
-  // check for 3 in a column
-  checkTiles(tiles[0], tiles[3], tiles[6])
-  checkTiles(tiles[1], tiles[4], tiles[7])
-  checkTiles(tiles[2], tiles[5], tiles[8])
-  // check for 3 diagonal
-  checkTiles(tiles[0], tiles[4], tiles[8])
-  checkTiles(tiles[2], tiles[4], tiles[6])
-  if (winner && moves === 9) {
-    alert('It\'s a tie!')
-  } else {
-    moves = moves + 1
-  }
+  var winningCombination = [ [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                             [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                             [0, 4, 8], [2, 4, 6]]
+  winningCombination.forEach(function (combo) {
+    if (!winner) {
+      winner = checkTiles(tiles[combo[0]], tiles[combo[1]], tiles[combo[2]])
+    }
+  })
 }
 
+// function to check whether 3 tiles contain the same mark
 function checkTiles (a, b, c) {
   if (a.textContent && b.textContent && c.textContent) {
     if ((a.textContent === b.textContent) && (a.textContent === c.textContent)) {
-      winner = a.textContent
-      alert('Winner is: ' + winner)
-    } else {
-      return false
+      return a.textContent
     }
   }
 }
 
-// function checkMark (tile) {
-//   console.log(tile.textContent)
-//   return tile.textContent
-// }
+// function to reset the game
+function resetBoard () {
+  player = true
+  moves = 1
+  winner = ''
+  updateTitle()
+  tiles.forEach(function (tile) {
+    tile.textContent = ''
+  })
+  reset.classList.add('hidden')
+}
+
+function updateTitle () {
+  player ? title.textContent = 'X\'s turn' : title.textContent = 'O\'s turn'
+}
